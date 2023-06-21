0164b1a9e875efd68685c43f7dc06cfcaa89dd28
FerranAgulloLopez <ferranagullolopez@gmail.com>
2023-06-21 10:45:57 +0200

    adds the option of ranged downloads

--- app/orchestrator/wsgi.py
+++ app/orchestrator/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
+from static_ranges import Ranges
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orchestrator.settings')
 
-application = get_wsgi_application()
+application = Ranges(get_wsgi_application())
